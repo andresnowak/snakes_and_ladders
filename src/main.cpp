b95d8c080f1cad27e471136392070409df1ea743
NKnowak <andresnowak52@gmail.com>
2020-05-30 22:42:13 -0500

    Now the program checks if the values inserted are correct

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,52 @@
 #include <string>
 #include <vector>
 
-void start_game(std::vector<int> &game_info, std::string &game_type)
+void printErrorMessageMenu(std::string adjective, std::string type, int min, int max)
+{
+    std::cout << adjective << " of " << type << " is incorrect, remember it has to be between " << min << " and " << max << " try again:" << std::endl;
+}
+
+bool isSizeCorrect(int &object, int &min, int &max)
+{
+    if (object >= min and object <= max)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int getNumberSizeOfVariable(int &variable, std::string adjective, std::string type, int &min, int &max)
+{
+    std::cin >> variable;
+
+    if (isSizeCorrect(variable, min, max))
+    {
+        return variable;
+    }
+
+    printErrorMessageMenu(adjective, type, min, max);
+
+    return getNumberSizeOfVariable(variable, adjective, type, min, max);
+}
+
+std::string getGameType()
+{
+    std::string game_type;
+    std::cin >> game_type;
+
+    if (game_type == "M" or game_type == "m" or game_type == "A" or game_type == "a")
+    {
+        return game_type;
+    }
+    std::cout << "The game type you inserted is incorrect, try again: " << std::endl;
+
+    return getGameType();
+}
+
+void startGame(std::vector<int> &game_info, std::string &game_type)
 {
     Game *game;
 
@@ -33,31 +78,44 @@ void menu()
 
     std::string game_type;
 
-    std::cout << "How many tiles you want: " << std::endl;
-    std::cin >> game_info[0];
+    std::cout << "How many tiles do you want: " << std::endl;
+    int min_tiles = 12;
+    int max_tiles = 100;
+    getNumberSizeOfVariable(game_info[0], "Size", "tiles", min_tiles, max_tiles);
 
-    std::cout << "How many snakes you want: " << std::endl;
-    std::cin >> game_info[1];
+    int min_snakes_ladders = 1;
+    int max_snakes_ladders = game_info[0] / 6;
 
-    std::cout << "How many ladders you want: " << std::endl;
-    std::cin >> game_info[2];
+    int min_reward_penalty = 2;
+    int max_reward_penalty = game_info[0] / 4;
 
-    std::cout << "How much reward you want: " << std::endl;
-    std::cin >> game_info[3];
+    std::cout << "How many snakes do you want: " << std::endl;
 
-    std::cout << "How much penalty you want: " << std::endl;
-    std::cin >> game_info[4];
+    getNumberSizeOfVariable(game_info[1], "Number", "snakes", min_snakes_ladders, max_snakes_ladders);
+
+    std::cout << "How many ladders do you want: " << std::endl;
+    getNumberSizeOfVariable(game_info[2], "Number", "ladders", min_snakes_ladders, max_snakes_ladders);
+
+    std::cout << "How big do you want the reward to be: " << std::endl;
+    getNumberSizeOfVariable(game_info[3], "Size", "reward", min_reward_penalty, max_reward_penalty);
+
+    std::cout << "How big do you want the penalty to be: " << std::endl;
+    getNumberSizeOfVariable(game_info[4], "Size", "penalty", min_reward_penalty, max_reward_penalty);
 
     std::cout << "How many players are going to play: " << std::endl;
-    std::cin >> game_info[5];
+    int min_players = 2;
+    int max_players = 6;
+    getNumberSizeOfVariable(game_info[5], "Number", "players", min_players, max_players);
 
-    std::cout << "What is the limit of turns you want: " << std::endl;
-    std::cin >> game_info[6];
+    std::cout << "What is the limit of turns that you want: " << std::endl;
+    int min_turns = 10;
+    int max_turns = INT32_MAX;
+    getNumberSizeOfVariable(game_info[6], "Number", "turns", min_turns, max_turns);
 
-    std::cout << "Do you want the game to be automatic or manual: " << std::endl;
-    std::cin >> game_type;
+    std::cout << "Do you want the game to be automatic or manual (A or M): " << std::endl;
+    game_type = getGameType();
 
-    start_game(game_info, game_type);
+    startGame(game_info, game_type);
 }
 
 int main()
