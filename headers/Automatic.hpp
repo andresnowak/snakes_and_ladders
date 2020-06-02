b59a0121f06d3ae887b0850ade194cc045375023
NKnowak <andresnowak52@gmail.com>
2020-06-01 22:32:50 -0500

    Eliminated automatic.cpp and manual.cpp, because it wasn't necessary to separate them from .hpp's

--- headers/Automatic.hpp
+++ headers/Automatic.hpp
@@ -1,6 +1,9 @@
 #include "Game.hpp"
 #include "Board.hpp"
 
+#include <chrono>
+#include <thread>
+
 #ifndef AUTOMATIC_H
 #define AUTOMATIC_H
 
@@ -12,12 +15,36 @@ private:
 
     std::queue<Player> *players_;
 
-    void play();
+    void play()
+    {
+        int board_size = board_->getSizeOfBoard();
+
+        int turns = 1;
+        while (true)
+        {
+            Game::play(turns, board_size);
+            std::this_thread::sleep_for(std::chrono::milliseconds(500)); // sleep multiplatform
+        }
+    };
 
 public:
-    Automatic(int, int, int, int, int, int, int);
+    Automatic(int tiles, int number_snakes, int number_ladders, int penalty, int reward, int number_players, int turns) : Game(tiles, number_snakes, number_ladders, penalty, reward, number_players, turns)
+    {
+        board_ = getBoard();
+        players_ = getPlayers();
+        dice_ = getDice();
+    }
 
-    void start();
+    void start()
+    {
+        std::cout << *board_ << std::endl;
+
+        std::cout << "Press C to start game, or E to end the game:" << std::endl;
+
+        choseOption();
+
+        play();
+    }
 };
 
 #endif
--- headers/Manual.hpp
+++ headers/Manual.hpp
@@ -14,12 +14,36 @@ private:
 
     std::queue<Player> *players_;
 
-    void play();
+    void play()
+    {
+        int board_size = board_->getSizeOfBoard();
+
+        int turns = 1;
+        while (true)
+        {
+            Game::play(turns, board_size);
+            choseOption();
+        }
+    }
 
 public:
-    Manual(int, int, int, int, int, int, int);
+    Manual(int tiles, int number_snakes, int number_ladders, int penalty, int reward, int number_players, int turns) : Game(tiles, number_snakes, number_ladders, penalty, reward, number_players, turns)
+    {
+        board_ = getBoard();
+        players_ = getPlayers();
+        dice_ = getDice();
+    }
 
-    void start();
+    void start()
+    {
+        std::cout << *board_ << std::endl;
+
+        std::cout << "Press C to continue next turn, or E to end the game:" << std::endl;
+
+        choseOption();
+
+        play();
+    }
 };
 
 #endif
